36b7bda3ddc605535696a118510489c188046216
Wolf-Tungsten <canis.tungsten@foxmail.com>
2019-05-08 23:42:52 +0800

    路由设计

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
-package go_common
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"go-common/handlers"
+)
 
 const ServerPort = "3001"
 
 func main() {
+
+	engine := gin.Default()
+	handlers.Handler(engine)
+	engine.Run(":" + ServerPort)
 
 }
